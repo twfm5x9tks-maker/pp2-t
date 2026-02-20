cb1fe18ae351b5c56ef79feb5c3f5b162636c857
EnlikZhuman <enlik.zhuman@icloud.com>
2026-02-20 11:25:05 +0500

    prac3

--- lab01/101.py
+++ lab01/101.py
@@ -1,4 +1,8 @@
-a="Hello, "
-b=input()
-
-print(a+b+"!")
+def val(number):
+    while number>0:
+        a=number%10
+        if a%2==0:
+            print("Valid")
+        else:
+            print("Not valid")
+        number//=10
