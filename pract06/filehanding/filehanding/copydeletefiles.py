522f1fa9c3618a73a39eae7a8c683173c415ec09
EnlikZhuman <enlik.zhuman@icloud.com>
2026-03-19 10:12:40 +0500

    update

--- pract06/filehanding/filehanding/copydeletefiles.py
+++ pract06/filehanding/filehanding/copydeletefiles.py
@@ -4,15 +4,19 @@ import os
 #1
 shutil.copy("file1.txt", "copy1.txt")
 
+
 #2
 shutil.copy2("file2.txt", "copy2.txt")
+
 
 #3
 shutil.copy("file3.txt", "backup_file3.txt")
 
+
 #4
 if os.path.exists("copy1.txt"):
     os.remove("copy1.txt")
+
 
 #5
 file_name = "copy2.txt"
